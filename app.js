6312d66da0e945d298029ff6540ae6ae72037f77
Harshaldalvi7797 <dalviharshal7797@gmail.com>
2023-04-04 08:32:05 +0530

    my test api

---
+++ app.js
@@ -0,0 +1,41 @@
+let express = require("express")
+let app = express()
+
+
+const PORT = 3000
+//port connection
+let server = app.listen(PORT, () => {
+    console.log(`connected to port ${PORT}`)
+});
+
+app.get("/url", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+   });
+
+   app.get("/matches",(req,res,next)=>
+   {
+    const response=[
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+          "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "qui est esse",
+          "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+          "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+        }]
+        res.send({data: response})
+   }
+   
+   )
+
+
